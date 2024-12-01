e89e8c05023b2290840a2539f56d3150197c066b
ÁlvaroFGA <alvarofredgonza2020@gmail.com>
2024-12-01 01:03:43 -0400

    medulo de diagnosticos completado

--- page_elements/pacientes/paciente.py
+++ page_elements/pacientes/paciente.py
@@ -2,14 +2,34 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.alert import Alert
+
 
 import time
+import string
+import random
 
+def genera_username():
+    alfabeto = list(string.ascii_lowercase)
+    numeros = list(range(1000, 10000))
+    user = ''.join([random.choice(alfabeto) for i in range(3)])  
+    number = str(random.choice(numeros)) 
+    username = user + number + "@gmail.com"  
+    return username
+
+def genera_ci():
+    numeros = list(range(100000, 99999999))  
+    number = str(random.choice(numeros)) 
+    username = number  
+    return username
+    
 class ModuloPaciente:
     def __init__(self, driver):
         self.driver = driver
-        self.email_def = 'test1@gmail.com'
-        self.ci_def = '1234567' 
+        self.email_def = genera_username()
+        self.ci_def = genera_ci() 
 
     def encabezado(self):
         actual = self.driver.find_element(By.XPATH, "//h2").text
@@ -55,54 +75,137 @@ class ModuloPaciente:
         time.sleep(10)
 
     def text_datos_personales(self):
-        try: 
-            boton = self.driver.find_element(By.XPATH, "//button[text()= 'DATOS PERSONALES']").is_displayed()
+        try:
+            boton = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//button[text()= 'DATOS PERSONALES']"))
+            )
+            return boton.is_displayed()
         except:
-            boton = False
-        finally:
-            return boton
+            return False
     
     def nav_bar_historial(self):
         self.driver.find_element(By.XPATH, "//button[text()= 'HISTORIAL CLINICO']").click()
-        time.sleep(1)
         try:
-            boton = self.driver.find_element(By.XPATH, "//button[text()= 'EDITAR DATOS DEL HISTORIAL']").is_displayed()
+            boton = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//button[text()= 'EDITAR DATOS DEL HISTORIAL']"))
+            )
+            return boton.is_displayed()
         except:
-            boton = False
-        finally:
-            return boton
-        
+            return False
+                
     def nav_bar_tratamientos(self):
         self.driver.find_element(By.XPATH, "//button[text()= 'TRATAMIENTOS']").click()
-        time.sleep(1)
         try:
-            boton = self.driver.find_element(By.XPATH, "//button[text()= 'CREAR NUEVO TRATAMIENTO']").is_displayed()
+            boton = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//button[text()= 'CREAR NUEVO TRATAMIENTO']"))
+            )
+            return boton.is_displayed()
         except:
-            boton = False
-        finally:
-            return boton
-        
+            return False
+            
     def nav_bar_prescripciones(self):
         self.driver.find_element(By.XPATH, "//button[text()= 'PRESCRIPCIÓNES']").click()
-        time.sleep(1)
         try:
-            boton = self.driver.find_element(By.XPATH, "//button[text()= 'CREAR NUEVA PRESCRIPCIÓN']").is_displayed()
+            boton = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//button[text()= 'CREAR NUEVA PRESCRIPCIÓN']"))
+            )
+            return boton.is_displayed()
         except:
-            boton = False
-        finally:
-            return boton
+            return False
     
     def editar_datos_personales(self):
         self.driver.find_element(By.XPATH, "//button[text()= 'EDITAR DATOS PERSONALES']").click()
         time.sleep(1)
-        self.driver.find_element(By.XPATH, "//label[text() = 'Dirección']//following-sibling::input").clear()
-        self.driver.find_element(By.XPATH, "//label[text() = 'Dirección']//following-sibling::input").send_keys('Edicion sqa')
+        dir = self.driver.find_element(By.XPATH, "//label[text() = 'Dirección']//following-sibling::input")
+        dir.clear()
+        dir.send_keys('Edicion sqa')
         time.sleep(1)
         self.driver.find_element(By.XPATH, "//button[@type = 'submit']").click()
-        time.sleep(3)
         try:
-            mensaje = self.driver.find_element(By.XPATH, "//div[text() = 'Datos personales actualizados.']").is_displayed()
+            mensaje = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Datos personales actualizados.']"))
+            )
+            return mensaje.is_displayed()
         except:
-            mensaje = False
-        finally:
-            return mensaje
+            return False
+        
+    def editar_datos_clinicos(self):
+        self.driver.find_element(By.XPATH, "//button[text()= 'EDITAR DATOS CLÍNICOS']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input[@placeholder = 'Ingrese alergias']").send_keys(genera_ci())
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[@type = 'submit']").click()
+        try:
+            mensaje = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Paciente actualizado.']"))
+            )
+            return mensaje.is_displayed()
+        except:
+            return False
+        
+    def descargar_historial(self):
+        self.boton_historial()
+        boton = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//button[text() = 'DESCARGAR HISTORIAL']"))
+            )
+        boton.click()
+        try:
+            mensaje = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, "//div[text() = 'Descarga exitosa']"))
+            )
+            return mensaje.is_displayed()
+        except:
+            return False
+        
+    def wait_locator(self, locator):
+        try:
+            elemento = WebDriverWait(self.driver, 15).until(
+                EC.visibility_of_element_located((By.XPATH, locator))
+            )
+            return elemento.is_displayed()
+        except:
+            return False
+        
+    def editar_datos_historial(self):
+        self.boton_historial()
+        self.driver.find_element(By.XPATH, "//button[text()= 'HISTORIAL CLINICO']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[text() = 'EDITAR DATOS DEL HISTORIAL']").click()
+        time.sleep(3)
+        self.driver.find_element(By.XPATH, "//textarea").send_keys('editado')
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[@type = 'submit']").click()
+        return self.wait_locator("//div[text() = 'Historial actualizado.']")
+    
+    def crear_diagnostico(self):
+        self.boton_historial()
+        self.driver.find_element(By.XPATH, "//button[text()= 'HISTORIAL CLINICO']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//button[text() = 'CREAR NUEVO DIAGNOSTICO']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//input").send_keys('prueba sqa')
+        self.driver.find_element(By.XPATH, "//textarea").send_keys('descripcion sqa')
+        self.driver.find_element(By.XPATH, "//button[text() = 'Crear']").click()
+        return self.wait_locator("//div[text() = 'Diagnóstico creado.']")
+
+    def editar_diagnostico(self):
+        self.boton_historial()
+        self.driver.find_element(By.XPATH, "//button[text()= 'HISTORIAL CLINICO']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//p[text() = 'PRUEBA SQA'][1]//following-sibling::button[text()]").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//textarea").send_keys(' editado')
+        self.driver.find_element(By.XPATH, "//button[text() = 'Guardar']").click()
+        return self.wait_locator("//div[text() = 'Diagnóstico actualizado.']")
+
+    def eliminar_diagnostico(self):
+        self.boton_historial()
+        self.driver.find_element(By.XPATH, "//button[text()= 'HISTORIAL CLINICO']").click()
+        time.sleep(1)
+        self.driver.find_element(By.XPATH, "//p[text() = 'PRUEBA SQA'][1]//following-sibling::button[@class = 'chakra-button css-17gj2bt']").click()
+        time.sleep(1)
+        alert = Alert(self.driver)
+        alert.accept()
+        return self.wait_locator("//div[text() = 'Diagnostico eliminado.']")
+
+
--- test_cases/test_modulo_paciente/test_paciente.py
+++ test_cases/test_modulo_paciente/test_paciente.py
@@ -22,31 +22,59 @@ class TestModulo:
         esperada = "PACIENTES"
         assert esperada == actual, f"ERROR, actual {actual}, esperado: {esperada}"
     
-    def test_crear_paciente(self):
+    def taest_crear_paciente(self):
         self.paciente.boton_crear()
         self.paciente.llenar_form_crear(True)
         self.paciente.enviar_form()
         creado = self.paciente.verificar_pacientecreado()
         assert creado, f'El paciente no se creo correctamente'
     
-    def test_ver_historial(self):
+    def taest_ver_historial(self):
         self.paciente.boton_historial()
         esperado = self.paciente.text_datos_personales()
         assert esperado, f'El boton de datos personales no se ve'
     
-    def test_historial_navbar_historial(self):
+    def taest_historial_navbar_historial(self):
         self.paciente.boton_historial()
-        self.paciente.nav_bar_historial()
+        esperado = self.paciente.nav_bar_historial()
+        assert esperado, f'El encabezado de historial no se ve'
 
-    def test_historial_navbar_tratamientos(self):
+    def taest_historial_navbar_tratamientos(self):
         self.paciente.boton_historial()
-        self.paciente.nav_bar_tratamientos()
+        esperado = self.paciente.nav_bar_tratamientos()
+        assert esperado, f'El encabezado de tratamientos no se ve'
 
-    def test_historial_navbar_prescripciones(self):
+    def taest_historial_navbar_prescripciones(self):
         self.paciente.boton_historial()
-        self.paciente.nav_bar_prescripciones()
+        esperado = self.paciente.nav_bar_prescripciones()
+        assert esperado, f'El encabezado de prescripciones no se ve'
 
-    def test_editar_datos_personales(self):
+    def taest_editar_datos_personales(self):
         self.paciente.boton_historial()
         mensaje = self.paciente.editar_datos_personales()
+        assert mensaje, f'El mensaje de exito no se muestra'
+
+    def taest_editar_datos_clinicos(self):
+        self.paciente.boton_historial()
+        mensaje = self.paciente.editar_datos_clinicos()
+        assert mensaje, f'El mensaje de exito no se muestra'
+
+    def taest_descargar_historial(self):
+        mensaje = self.paciente.descargar_historial()
+        assert mensaje, f'El mensaje de exito no se muestra'
+        
+    def taest_editar_datos_historial(self):
+        mensaje = self.paciente.editar_datos_historial()
+        assert mensaje, f'El mesanje de exito no se muestra'
+
+    def taest_crear_daignostico(self):
+        mensaje = self.paciente.crear_diagnostico()
+        assert mensaje, f'El mensaje de exito no se muestra'
+    
+    def taest_editar_daignostico(self):
+        mensaje = self.paciente.editar_diagnostico()
+        assert mensaje, f'El mensaje de exito no se muestra'
+        
+    def taest_eliminar_diagnostico(self):
+        mensaje = self.paciente.eliminar_diagnostico()
         assert mensaje, f'El mensaje de exito no se muestra'
