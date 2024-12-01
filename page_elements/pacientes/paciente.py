c2c9a1c0ec65e2476a271b18e8eb858fa2154897
ÁlvaroFGA <alvarofredgonza2020@gmail.com>
2024-12-01 01:05:17 -0400

    medulo de diagnosticos completado

--- page_elements/pacientes/paciente.py
+++ page_elements/pacientes/paciente.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.alert import Alert
