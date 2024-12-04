849a64e92db8ddbd95f08d973f5f4fde2f90fe29
Israel Pari <israelpari@Ordenador-portatil-de-Israel.local>
2024-12-03 21:41:22 -0400

    Arreglos

--- test_cases/test_pagina_inicio/test_landing_page.py
+++ test_cases/test_pagina_inicio/test_landing_page.py
@@ -15,32 +15,29 @@ class TestInterfaz:
         self.driver.quit()
         print("Prueba completada")
 
-    def button_about(self):
+    def test_verify_About_button(self):
 
         self.driver.find_element(By.XPATH, "//p[contains(text(),'Acerca ')]//parent::div[@class='css-1cu9hd1']//child::p[contains(text(),'Acerca')]").click()
         time.sleep(2)
-
         actual = self.driver.find_element(By.XPATH, "//p[contains(text(),'en la atención personalizada para cada paciente. Ofrecemos una amplia gama de servicios, que incluyen')]//parent::div//child::p[@class='chakra-text css-rszk63']").text
         esperada = "en la atención personalizada para cada paciente. Ofrecemos una amplia gama de servicios, que incluyen tratamientos preventivos"
 
 
         assert esperada in actual, f"ERROR, actual {actual}, esperado: {esperada}"
 
-    def button_service(self):
-        self.driver.find_elemnt(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/button").click()
-        actual = self.driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/button").text()
-        time.sleep(2)
+    
+    def test_verify_Servicios_button(self):
+
+        self.driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/button").click()
+
+        actual = self.driver.find_element(By.XPATH,"//*[@id='services-section']/div/div/div/div[1]/p").text
+        time.sleep(4)
+
         esperado = "ofrecemos una amplia gama de servicios dentales para satisfacer todas tus necesidades de salud bucal"
         assert esperado in actual,f"ERROR, actual {actual}, esperado: {esperado}"
-
-    
-
-    def bun_ser(self):
-        self.driver.find_elemnt(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/button").click()
-        actual = self.driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/button").text()
+    def test_verify_Contacto_button(self):
+        self.driver.find_element(By.XPATH,"//*[@id='root']/div/div[1]/div[1]/div[3]/p[2]").click()
+        actual = self.driver.find_element(By.XPATH,"//*[@id='chakra-modal--body-:r2:']/p[2]").text
         time.sleep(2)
-        esperado = "ofrecemos una amplia gama de servicios dentales para satisfacer todas tus necesidades de salud bucal"
+        esperado="Correo electrónico"
         assert esperado in actual,f"ERROR, actual {actual}, esperado: {esperado}"
-
-    
-
